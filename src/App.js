5c4d1e1bc45c5c85a749bbbb75a467ab59bad6f4
hungbkvn9x1@gmail.com <hungbkvn9x1@gmail.com>
2023-05-29 17:57:50 +0700

    main

--- src/App.js
+++ src/App.js
@@ -1,22 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Nav from './component/Nav';
 
-function App() {
+const App = () => {
+  const [name, setName] = useState('')
+  const [todos, setTodos] = useState([])
+
+  const handleEvenClick = (event) => {
+    if (!name) {
+      alert("Input empry");
+      return;
+    }
+    setTodos([...todos, name])
+    setName("")
+  }
+  const handleOnChange = (event) => {
+    setName(event.target.value)
+  }
   return (
     <div className="App">
+      <Nav />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello World!!!</h1>
+        <input type="text" value={name} onChange={handleOnChange} />
+        <button type="button" onClick={handleEvenClick}>Click me</button>
+        {
+          todos && todos.length > 0 &&
+          todos.map((item, index) => {
+            return (
+              <div key={index}>{item}</div>
+            )
+          })
+
+        }
       </header>
     </div>
   );
